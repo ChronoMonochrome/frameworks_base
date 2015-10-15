498d33fc9cf0608f26c291e0ee3986f845ce8a29
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-15 11:02:25 +0300

    Revert "Add support for max-res decode"
    This reverts commit 336d3eabc0d57fd2d1b8a9aa4ade7a0450cf183e.

--- media/jni/android_media_MediaCodec.cpp
+++ media/jni/android_media_MediaCodec.cpp
@@ -62,7 +62,6 @@ static struct CryptoErrorCodes {
     jint cryptoErrorNoKey;
     jint cryptoErrorKeyExpired;
     jint cryptoErrorResourceBusy;
-    jint cryptoErrorInsufficientOutputProtection;
 } gCryptoErrorCodes;
 
 static struct CodecActionCodes {
@@ -757,9 +756,6 @@ static void throwCryptoException(JNIEnv *env, status_t err, const char *msg) {
         case ERROR_DRM_RESOURCE_BUSY:
             err = gCryptoErrorCodes.cryptoErrorResourceBusy;
             break;
-        case ERROR_DRM_INSUFFICIENT_OUTPUT_PROTECTION:
-            err = gCryptoErrorCodes.cryptoErrorInsufficientOutputProtection;
-            break;
         default:  /* Other negative DRM error codes go out as is. */
             break;
     }
@@ -1436,11 +1432,6 @@ static void android_media_MediaCodec_native_init(JNIEnv *env) {
     field = env->GetStaticFieldID(clazz.get(), "ERROR_RESOURCE_BUSY", "I");
     CHECK(field != NULL);
     gCryptoErrorCodes.cryptoErrorResourceBusy =
-        env->GetStaticIntField(clazz.get(), field);
-
-    field = env->GetStaticFieldID(clazz.get(), "ERROR_INSUFFICIENT_OUTPUT_PROTECTION", "I");
-    CHECK(field != NULL);
-    gCryptoErrorCodes.cryptoErrorInsufficientOutputProtection =
         env->GetStaticIntField(clazz.get(), field);
 
     clazz.reset(env->FindClass("android/media/MediaCodec$CodecException"));
